cbd4a41be9fdc28129420664b225f18aad72675e
watataku8911 <watato.takuya@gmail.com>
2021-08-09 14:46:16 +0900

    first-commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# lineBot-sample
+
+[Line Developers](https://developers.line.biz/ja/)
+
+## 必要なモジュール
+
+```
+$ npm install exxpress @line/bot-sdk node-dev --save
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const express = require("express");
+const line = require("@line/bot-sdk");
+const PORT = process.env.PORT || 3000;
+
+const getChannelSeacret = require("./seacretDirectory/channelSeacret");
+const getChannelAccessToken = require("./seacretDirectory/channelAccessToken");
+
+const config = {
+  channelSecret: getChannelSeacret(),
+  channelAccessToken: getChannelAccessToken(),
+};
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello LINE BOT!(GET)")); //ブラウザ確認用(無くても問題ない)
+app.post("/webhook", line.middleware(config), (req, res) => {
+  console.log(req.body.events);
+
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
+});
+
+const client = new line.Client(config);
+
+async function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  /* -------------------------------------------
+    -------返信メッセージの処理の記述ゾーン ---------
+    ------------------------------------------- */
+
+  let replyText = "";
+  replyText = event.message.text; // event.message.textに自分のメッセージが入っている。
+
+  /* ------------------------------------------
+    ------------------------------------------
+    ------------------------------------------ */
+
+  /* ここでメッセージを返す。(replyTextに返すメッセージが入っている。) */
+
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: replyText,
+  });
+}
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
